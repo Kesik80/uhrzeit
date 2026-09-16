d1d2afaa21c0bfe6aaef4324435cb7f683b9fad8
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-09-16 19:59:17 +0200

    Upload 3 file(s)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Нужен Chrome для установки и даёт работу без сети.
    Стратегия «сначала сеть»: после деплоя всегда свежие файлы,
    кэш используется только когда сети нет. */
-const CACHE = 'uhrzeit-v1';
+const CACHE = 'uhrzeit-v2';
 const PRECACHE = ['/', '/index.html', '/install.js', '/icons/manifest.json', '/icons/icon-192.png'];
 
 self.addEventListener('install', (e) => {
